1ea5ee13984d161abaf3789b66d867bd37bd0809
adrianapereira27 <pereira.adri@gmail.com>
2024-05-15 10:34:03 -0300

    Add project files.

---
+++ Modulo1.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Informe seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Informe sua idade: ");
+int idade = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Informe seu cargo: ");
+string cargo = Console.ReadLine();
+
+DateTime now = DateTime.Now;
+String hoje = now.ToString("G");
+
+Console.WriteLine($"Olá, bem vindo {nome}, você tem {idade} anos, seu cargo é {cargo}, hoje é {hoje}");
+
+Console.ReadKey();
